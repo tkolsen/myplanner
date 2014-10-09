c0939d1d73ba27faf693645ca537cfd443da7652
tkolsen <tom.k.olsen@gmail.com>
2014-10-09 20:24:32 +0200

    test

---
+++ src/main/java/RedirectServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by TomKolse on 09-Oct-14.
+ */
+public class RedirectServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("doGet()");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("doPost()");
+    }
+}
---
+++ src/main/java/TestOltu.java
@@ -0,0 +1,30 @@
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by TomKolse on 09-Oct-14.
+ */
+public class TestOltu extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        OAuthClientRequest request = null;
+        try {
+            request = OAuthClientRequest
+                    .authorizationLocation("https://learn-lti.herokuapp.com/login/oauth2/auth")
+                    .setClientId("1105")
+                    .setRedirectURI("http://localhost:8080/redirect")
+                    .setResponseType("code")
+                    .buildQueryMessage();
+            resp.sendRedirect(request.getLocationUri());
+        } catch (OAuthSystemException e) {
+            e.printStackTrace();
+        }
+    }
+}
