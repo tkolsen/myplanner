88de03e35a72a43ce85cef0985bde31c10030994
tkolsen <tom.k.olsen@gmail.com>
2014-10-09 16:52:11 +0200

    added selenium and cucumber dependencies

--- src/test/java/RunTests.java
+++ src/test/java/RunTests.java
@@ -1,5 +1,10 @@
-/**
- * Created by Kim on 02.10.2014.
- */
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        format = {"pretty"}
+)
 public class RunTests {
 }
