b86e5aac94597e6318cd8f233d0598be722433da
Kim Andre Kjelsberg <kakjels@gmail.com>
2014-10-09 16:21:35 +0200

    Added Scribe dependencies for handling Canvas OAuth

---
+++ src/main/java/CanvasApi.java
@@ -0,0 +1,19 @@
+import org.scribe.builder.api.DefaultApi20;
+import org.scribe.model.OAuthConfig;
+
+/**
+ * Created by Kim on 09.10.2014.
+ */
+public class CanvasApi extends DefaultApi20{
+    @Override
+    public String getAccessTokenEndpoint() {
+        return null;
+        // TODO: Implement this shit!
+    }
+
+    @Override
+    public String getAuthorizationUrl(OAuthConfig oAuthConfig) {
+        return null;
+        // TODO: Implement this shit!
+    }
+}
