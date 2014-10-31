b2235ad8105c0f4d702cb2a26ed4921a64d21e07
tkolsen <tom.k.olsen@gmail.com>
2014-10-31 15:13:03 +0100

    Funker med knp

--- src/main/java/MyPlanner/controller/HomeController.java
+++ src/main/java/MyPlanner/controller/HomeController.java
@@ -12,6 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.social.oauth2.AccessGrant;
 import org.springframework.social.oauth2.OAuth2Parameters;
@@ -38,24 +41,32 @@ public class HomeController {
         template = new OAuth2Template(env.getProperty("client.id"), env.getProperty("client.secret"),
                 env.getProperty("provider.authorizeUrl"), env.getProperty("provider.accessTokenUrl"));
         OAuth2Parameters params = new OAuth2Parameters();
-        String authUrl = template.buildAuthenticateUrl(params);
+        params.setScope("profile");
+        String authUrl = template.buildAuthorizeUrl(params);
         response.sendRedirect(authUrl);
     }
 
     @RequestMapping(value="/oauth/redirect")
     public ModelAndView oauthRedirect(HttpServletResponse response, HttpServletRequest request) throws IOException{
+        OAuth2Parameters params = new OAuth2Parameters();
+        AccessGrant grant = template.exchangeForAccess(request.getParameter("code"), env.getProperty("client.redirect"), params);
 
-
+        response.addCookie(new Cookie("at", grant.getAccessToken()));
         return new ModelAndView("oauth");
     }
 
     @RequestMapping(value="/oauth/collect-eggs")
     public void collectEggs(HttpServletResponse response, @CookieValue("at") String token) throws IOException{
         RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        System.out.println(token);
+        headers.add("Authorization", "Bearer " + token);
+        HttpEntity entity = new HttpEntity(headers);
 
-        HashMap<String, String> params = new HashMap<String, String>();
-        params.put("access_token", token);
-        CustomRestResponse resp = restTemplate.getForObject("http://coop.apps.knpuniversity.com/api/me", CustomRestResponse.class, params);
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("scope", env.getProperty("scope.profile"));
+        HttpEntity<String> resp = restTemplate.exchange("http://coop.apps.knpuniversity.com/api/me", HttpMethod.GET, entity,String.class, params);
         System.out.println(resp.toString());
+
     }
 }
