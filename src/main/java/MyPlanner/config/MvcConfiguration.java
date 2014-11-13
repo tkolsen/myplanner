05e5ed80262f2158f58907feed2048dd345fb3ad
tkolsen <tom.k.olsen@gmail.com>
2014-11-13 19:45:14 +0100

    added sample spring security

--- src/main/java/MyPlanner/config/MvcConfiguration.java
+++ src/main/java/MyPlanner/config/MvcConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@ComponentScan(basePackages="MyPlanner")
+@ComponentScan//(basePackages="MyPlanner")
 @PropertySource("/WEB-INF/mp.properties")
 @EnableWebMvc
 public class MvcConfiguration extends WebMvcConfigurerAdapter{
---
+++ src/main/java/MyPlanner/oauth/OAuth.java
@@ -0,0 +1,5 @@
+package MyPlanner.oauth;
+
+public class OAuth {
+
+}
