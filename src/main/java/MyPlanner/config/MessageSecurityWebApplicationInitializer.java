4948aeaee6651f471e5a66aa81fe7a4234b981ac
tkolsen <tom.k.olsen@gmail.com>
2014-11-13 19:53:08 +0100

    added MessageSecurity.. class

---
+++ src/main/java/MyPlanner/config/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package MyPlanner.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MessageSecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+}
