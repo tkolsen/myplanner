32093f501eddb5122ec4a8f35cd4087f25858246
tkolsen <tom.k.olsen@gmail.com>
2014-11-21 19:23:40 +0100

    testing resttemplate again

--- src/main/java/MyPlanner/controller/TestController.java
+++ src/main/java/MyPlanner/controller/TestController.java
@@ -3,14 +3,23 @@ package MyPlanner.controller;
 import MyPlanner.oauth.OAuth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.social.oauth2.OAuth2Template;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/testing")
@@ -27,10 +36,27 @@ public class TestController {
 
     @RequestMapping("/redirect")
     public void redirect(HttpServletRequest request, HttpServletResponse response) throws InstantiationException, IOException {
-        String code = request.getParameter("code");
+        /*String code = request.getParameter("code");
         oAuth.exchangeCodeForToken(code, request);
 
-        response.sendRedirect("/ok");
+        response.sendRedirect("/ok");*/
+
+        String code = request.getParameter("code");
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        Map<String, String> parameters = new HashMap<String, String>();
+        parameters.put("client_id", env.getProperty("client.id"));
+        parameters.put("client_secret", env.getProperty("client.secret"));
+        parameters.put("redirect_uri", env.getProperty("client.redirect"));
+        parameters.put("code", code);
+        restTemplate.exchange(env.getProperty("provider.accessTokenUrl"), HttpMethod.POST, entity, String.class, parameters);
+        String body = entity.getBody();
+        System.out.println(body);
+
+        response.sendRedirect("/testing/ok");
     }
 
     @RequestMapping("/ok")
