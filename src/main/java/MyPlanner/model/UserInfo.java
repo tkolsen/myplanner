4f9022a45c7aa21574bdb4ff991e2c7a7c03ab26
tkolsen <tom.k.olsen@gmail.com>
2014-11-22 19:29:34 +0100

    added json property to model class

--- src/main/java/MyPlanner/model/UserInfo.java
+++ src/main/java/MyPlanner/model/UserInfo.java
@@ -1,8 +1,21 @@
 package MyPlanner.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class UserInfo {
+    @JsonProperty("name")
     private String name;
+    @JsonProperty("id")
     private String id;
+
+    public UserInfo(){
+        
+    }
+
+    public UserInfo(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
 
     public String getName() {
         return name;
