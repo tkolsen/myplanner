e838a82348029b413dc734437e6d8e0b91d750b1
tkolsen <tom.k.olsen@gmail.com>
2014-11-24 21:19:27 +0100

    .

--- src/main/java/MyPlanner/config/MvcConfiguration.java
+++ src/main/java/MyPlanner/config/MvcConfiguration.java
@@ -12,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.awt.*;
 import java.util.Properties;
 
 @Configuration
