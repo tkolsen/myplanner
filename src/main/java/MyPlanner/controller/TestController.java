d8ad5430a4131bf924f264d1768710bbe88833bf
tkolsen <tom.k.olsen@gmail.com>
2014-11-24 19:00:27 +0100

    testing how to get token

--- src/main/java/MyPlanner/controller/TestController.java
+++ src/main/java/MyPlanner/controller/TestController.java
@@ -6,6 +6,10 @@ import MyPlanner.oauth.OAuth;
 import MyPlanner.service.LoginInfoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.social.oauth2.AccessGrant;
+import org.springframework.social.oauth2.GrantType;
+import org.springframework.social.oauth2.OAuth2Parameters;
+import org.springframework.social.oauth2.OAuth2Template;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,6 +28,8 @@ public class TestController {
     @Autowired
     LoginInfoRepo loginInfoRepo;
 
+    OAuth2Template oAuth2Template;
+
     @RequestMapping("/login")
     public void login(HttpServletResponse response) throws IOException, InstantiationException {
         oAuth.askForConfirmation(response);
@@ -37,10 +43,34 @@ public class TestController {
             request.getSession().setAttribute("loginInfo", userInfo);
             loginInfoRepo.saveUser(userInfo);
 
-            return "redirect:/user/profile";
+            return "redirect:/testing/token";
         }else {
             throw new NotAuthorizedException("Some info values where not set");
         }
+    }
+
+    @RequestMapping("/token")
+    public void getToken(HttpServletResponse response) throws IOException {
+        OAuth2Parameters oAuth2Parameters = new OAuth2Parameters();
+        oAuth2Parameters.setRedirectUri("http://myplanner.aitel.hist.no/MyPlanner/testing/redirect2");
+
+        oAuth2Template = new OAuth2Template(env.getProperty("client.id"), env.getProperty("client.secret"),
+                env.getProperty("provider.authorizeUrl"), env.getProperty("provider.accessTokenUrl"));
+
+        String codeUrl = oAuth2Template.buildAuthorizeUrl(GrantType.AUTHORIZATION_CODE, oAuth2Parameters);
+        response.sendRedirect(codeUrl);
+    }
+
+    @RequestMapping("/redirect2")
+    public String redirect2(HttpServletResponse response, HttpServletRequest request){
+        AccessGrant accessGrant = oAuth2Template.exchangeForAccess(request.getParameter("code"), "http://myplanner.aitel.hist.no/MyPlanner/testing/redirect2", null);
+        String accessToken = accessGrant.getAccessToken();
+
+        LoginInfo loginInfo = (LoginInfo)request.getSession().getAttribute("loginInfo");
+        loginInfo.setAccessToken(accessToken);
+        request.getSession().setAttribute("loginInfo", loginInfoRepo);
+
+        return "redirect:/user/profile";
     }
 
 }
