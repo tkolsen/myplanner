0b3216ba9b4c3f43011e62c8dd50bfe627643160
Kim Andre Kjelsberg <kakjels@gmail.com>
2014-12-10 17:27:39 +0100

    test-class added, testing not implemented

---
+++ src/test/java/MyPlanner/utils/ScheduleGeneratorTest.java
@@ -0,0 +1,35 @@
+package MyPlanner.utils;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScheduleGeneratorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGenerateSchedule() throws Exception {
+
+    }
+
+    @Test
+    public void testGenerateSchedule1() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateSchedule() throws Exception {
+
+    }
+}
